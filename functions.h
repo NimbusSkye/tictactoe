902b4d99b95b39f9c847e981f8ea7082afadadba
nimbusskye <alexwang6671@gmail.com>
2020-02-09 18:38:44 -0500

    Optimized include statements in functions.h

--- functions.h
+++ functions.h
@@ -1,15 +1,14 @@
+#ifndef TICTACTOE_FUNCTIONS_H
+#define TICTACTOE_FUNCTIONS_H
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <string>
 #include <cstring>
-
-#ifndef TICTACTOE_FUNCTIONS_H
-#define TICTACTOE_FUNCTIONS_H
+#endif //TICTACTOE_FUNCTIONS_H
 bool checkIfLegal (int cellNbre, char board[]);
 bool checkWinner (char board[]);
 void playerMove(char board[]);
 void displayBoard (char board[]);
 void greetAndInstruct();
 void computerMove(char board[]);
-#endif //TICTACTOE_FUNCTIONS_H
