d37b108b3d8b71973916340f33aff316b62aa247
nimbusskye <alexwang6671@gmail.com>
2020-02-08 21:29:20 -0500

    Added functions.cpp and functions.h

---
+++ functions.h
@@ -0,0 +1,16 @@
+//
+// Created by Admin on 2/8/2020.
+//
+
+#ifndef TICTACTOE_FUNCTIONS_H
+#define TICTACTOE_FUNCTIONS_H
+
+#endif //TICTACTOE_FUNCTIONS_H
+
+bool checkIfLegal(int cellNbre, char board[]);
+void displaySpaces (int a, int b, char board[]);
+bool checkWinner (char board[]);
+void displayBoard (char board[]);
+void greetAndInstruct();
+
+
